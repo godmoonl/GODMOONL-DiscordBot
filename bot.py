94d54ec14c8b7df4f557b0b5c4ee2632139ef18a
godmoonl <himoon345@gmail.com>
2019-05-05 12:28:05 +0900

    color update

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ async def on_message(message):
     if message.content == '!도움':
         r = open('update.txt',mode='rt',encoding = 'utf-8')
         rows = r.readlines()
-        e = embed(title = '갓봇 도움!')
+        e = embed(title = '갓봇 도움!',description = '갓봇 명령어를 불러옵니다',color=0x00C853)
         for i in rows:
             e.add_field(name = i.split('/')[0],value=i.split('/')[1])
         await app.send_message(message.channel,embed=e)
@@ -48,7 +48,7 @@ async def on_message(message):
         hour = str(hour%24)
         ut=str(ut%60)
         min=str(min%60)
-        e = embed(title="갓봇 정보!", description="개발자 : GODMOONL#7059\n업타임 : "+day+"일 "+hour+"시간 "+min+"분 "+ut+"초 ", color=0x00ff00)
+        e = embed(title="갓봇 정보!", description="개발자 : GODMOONL#7059\n업타임 : "+day+"일 "+hour+"시간 "+min+"분 "+ut+"초 ", color=0x00C853)
         await app.send_message(message.channel,embed=e)
         
     for i in range(0,2):
@@ -62,22 +62,22 @@ async def on_message(message):
     if message.content.startswith('!골라 '): 
         tmp = message.content.split('!골라 ')[1]
         ans = random.choice(tmp.split('/'))
-        e = embed(title="갓봇의 선택은?",description=ans)
+        e = embed(title="갓봇의 선택은?",description=ans,color=0x00C853)
         await app.send_message(message.channel,embed=e)
 
     if message.content.startswith('!확률 '):
         ans = str(random.randrange(0,100))
         q = message.content.split('!확률 ')[1]
-        e = embed(title=q+"은?",description=ans+"%입니다")
+        e = embed(title=q+"은?",description=ans+"%입니다",color=0x00C853)
         await app.send_message(message.channel,embed=e)
 
     if message.content == '!프사':
-        e = embed(title="당신의 프로필 사진")
+        e = embed(title="당신의 프로필 사진",color=0x00C853)
         e.set_image(url=message.author.avatar_url)
         await app.send_message(message.channel,embed=e)    
 
     if message.content.startswith('!프사 '):
-        e = embed(title="맨션한 사용자의 프로필 사진")
+        e = embed(title="맨션한 사용자의 프로필 사진",color=0x00C853)
         if not message.mentions:
             e = embed(title="에러!",description="에러 발생")
             await app.send_message(message.channel,embed=e)
@@ -89,7 +89,7 @@ async def on_message(message):
     if message.content == '!돈순위':	
         cur.execute('SELECT * FROM users ORDER BY money DESC')	
         l = cur.fetchall()	
-        e = embed(title = "돈순위!",description="돈순위 상위 5명을 불러옵니다")	
+        e = embed(title = "돈순위!",description="돈순위 상위 5명을 불러옵니다",color=0x00C853)	
         for i in range(0,5):	
             e.add_field(name =str(i+1)+'위',value='<@%s>\n%d원'%(l[i][1],l[i][0]))	
         await app.send_message(message.channel,embed=e)	
@@ -111,9 +111,9 @@ async def on_message(message):
             cur.execute('UPDATE users SET money = ?, time=? WHERE id = ?',(m,time.time(),uid))	
             conn.commit()	
         if m=="":	
-            e = embed(title="오류",description='돈은 5분에 한번씩 받을 수 있습니다 \n'+str(int((l[2]+300)-time.time()))+'초 남았습니다')	
+            e = embed(title="오류",description='돈은 5분에 한번씩 받을 수 있습니다 \n'+str(int((l[2]+300)-time.time()))+'초 남았습니다',color=0xF44336)	
         else:	
-            e = embed(title = "돈을 받았습니다.",description = '당신의 돈은 '+m+'원입니다')	
+            e = embed(title = "돈을 받았습니다.",description = '당신의 돈은 '+m+'원입니다',color=0x00C853)	
         await app.send_message(message.channel,embed=e)
 
     if message.content.startswith('!위키 '):
