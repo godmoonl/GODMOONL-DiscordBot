897c252600e4ba77da5bb3bcd3119621080a2664
godmoonl <himoon345@gmail.com>
2019-05-06 00:53:13 +0900

    !날씨 명령어 보기 좋게 수정

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ def wiki(message):
     url = 'https://ko.wikipedia.org/wiki/'+ location
     try:
         html = urlopen(url)
-    except HTTPError as e:
+    except HTTPError:
         return 0
     soup = BeautifulSoup(html,'html.parser')
     ans = soup.find('div', class_='mw-parser-output').find('p').text
@@ -21,30 +21,39 @@ def namu(message):
     req = urllib.request.Request(url,headers=hdr)
     response = urlopen(req)
     soup = BeautifulSoup(response,'html.parser')
-    ans = soup.find('div',class_='wiki-heading-content').text
-    return ans+'\n'+url
+    ans = soup.find('div',class_='wiki-heading-content').text[:500]
+    return ans+'...\n'+url
 
 def enwiki(message):
     location = urllib.parse.quote(message)
     url = 'https://en.wikipedia.org/wiki/'+ location
     try:
         html = urlopen(url)
-    except HTTPError as e:
+    except HTTPError:
         return 0
     soup = BeautifulSoup(html,'html.parser')
     ans = soup.find('div', class_='mw-parser-output').find('p').text
     return ans+'\n'+url  
-
-def weather(message):
-    location = urllib.parse.quote(message+'+날씨')
-    url = 'https://search.naver.com/search.naver?ie=utf8&query='+ location
-    try:
+class Weather:
+    def __init__(self,message):
+        location = urllib.parse.quote(message+'+날씨')
+        url = 'https://search.naver.com/search.naver?ie=utf8&query='+ location
         html = urlopen(url)
-    except HTTPError as e:
-        return 0
-    soup = BeautifulSoup(html,'html.parser')
-    ans = soup.find('div', class_='info_data').find('p',class_='info_temperature').text+'\n'+soup.find('div',class_='detail_box').text
-    return ans 
+        soup = BeautifulSoup(html,'html.parser')
+        self.temp = soup.find('span', class_='todaytemp').text
+        aq = soup.find('dl',class_='indicator').text.split(' ')
+        self.aq = aq
+        aqt = aq[2].split('㎍/㎥')[1]
+        if aqt == '좋음':
+            c = 0x32a1ff
+        elif aqt == '보통':
+            c = 0x00a83d
+        elif aqt == '나쁨':
+            c = 0xfd9b5a
+        else:
+            c = 0xff5959
+        self.color = c
+        
 class Melon:
     def __init__(self):
         url = 'http://www.melon.com/chart/index.htm'
