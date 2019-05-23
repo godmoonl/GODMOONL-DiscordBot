0e727d14238c12dda7387452338f3e73411ea0c2
godmoonl <himoon345@gmail.com>
2019-05-23 16:12:17 +0900

    ㅁㄴㅇㄹ

--- bot.py
+++ bot.py
@@ -35,9 +35,10 @@ async def on_message(message):
     if message.content == '!도움':
         r = open('update.txt',mode='rt',encoding = 'utf-8')
         rows = r.readlines()
-        e = embed(title = '갓봇 도움!')
+        e = embed(title = '갓봇 도움!',description = '갓봇 명령어를 불러옵니다',color=0x00C853)
         for i in rows:
             e.add_field(name = i.split('/')[0],value=i.split('/')[1])
+        e.set_footer(text='개발자 : GODMOONL#7059')
         await app.send_message(message.channel,embed=e)
 
     if message.content == '!정보':
@@ -49,7 +50,7 @@ async def on_message(message):
         hour = str(hour%24)
         ut=str(ut%60)
         min=str(min%60)
-        e = embed(title="갓봇 정보!", description="개발자 : GODMOONL#7059\n업타임 : "+day+"일 "+hour+"시간 "+min+"분 "+ut+"초 ", color=0x00ff00)
+        e = embed(title="갓봇 정보!", description="개발자 : GODMOONL#7059\n업타임 : "+day+"일 "+hour+"시간 "+min+"분 "+ut+"초 ", color=0x00C853)
         await app.send_message(message.channel,embed=e)
         
     for i in range(0,2):
@@ -63,22 +64,22 @@ async def on_message(message):
     if message.content.startswith('!골라 '): 
         tmp = message.content.split('!골라 ')[1]
         ans = random.choice(tmp.split('/'))
-        e = embed(title="갓봇의 선택은?",description=ans)
+        e = embed(title="갓봇의 선택은?",description=ans,color=0x00C853)
         await app.send_message(message.channel,embed=e)
 
     if message.content.startswith('!확률 '):
         ans = str(random.randrange(0,100))
         q = message.content.split('!확률 ')[1]
-        e = embed(title=q+"은?",description=ans+"%입니다")
+        e = embed(title=q+"은?",description=ans+"%입니다",color=0x00C853)
         await app.send_message(message.channel,embed=e)
 
     if message.content == '!프사':
-        e = embed(title="당신의 프로필 사진")
+        e = embed(title="당신의 프로필 사진",color=0x00C853)
         e.set_image(url=message.author.avatar_url)
         await app.send_message(message.channel,embed=e)    
 
     if message.content.startswith('!프사 '):
-        e = embed(title="맨션한 사용자의 프로필 사진")
+        e = embed(title="맨션한 사용자의 프로필 사진",color=0x00C853)
         if not message.mentions:
             e = embed(title="에러!",description="에러 발생")
             await app.send_message(message.channel,embed=e)
@@ -90,7 +91,7 @@ async def on_message(message):
     if message.content == '!돈순위':	
         cur.execute('SELECT * FROM users ORDER BY money DESC')	
         l = cur.fetchall()	
-        e = embed(title = "돈순위!",description="돈순위 상위 5명을 불러옵니다")	
+        e = embed(title = "돈순위!",description="돈순위 상위 5명을 불러옵니다",color=0x00C853)	
         for i in range(0,5):	
             e.add_field(name =str(i+1)+'위',value='<@%s>\n%d원'%(l[i][1],l[i][0]))	
         await app.send_message(message.channel,embed=e)	
@@ -112,16 +113,64 @@ async def on_message(message):
             cur.execute('UPDATE users SET money = ?, time=? WHERE id = ?',(m,time.time(),uid))	
             conn.commit()	
         if m=="":	
-            e = embed(title="오류",description='돈은 5분에 한번씩 받을 수 있습니다 \n'+str(int((l[2]+300)-time.time()))+'초 남았습니다')	
+            e = embed(title="오류",description='돈은 5분에 한번씩 받을 수 있습니다 \n'+str(int((l[2]+300)-time.time()))+'초 남았습니다',color=0xF44336)	
         else:	
-            e = embed(title = "돈을 받았습니다.",description = '당신의 돈은 '+m+'원입니다')	
+            e = embed(title = "돈을 받았습니다.",description = '당신의 돈은 '+m+'원입니다',color=0x00C853)	
         await app.send_message(message.channel,embed=e)
 
     if message.content.startswith('!위키 '):
         m = message.content[4:]
         ans = test.wiki(m)
-        e = embed(title = m+" 검색결과")
-        e.add_field(name = '내용',value = ans)
+        if not ans:
+            e = embed(title = '오류',description = '위키피디아에 없는 문서입니다')
+            await app.send_message(message.channel,embed=e)
+        else:
+            e = embed(title = m+" 검색결과")
+            e.add_field(name = '내용',value = ans)
+            e.set_footer(text='ko.wikipedia.org 위키피디아')
+            await app.send_message(message.channel,embed=e)
+    if message.content.startswith('!나무위키 '):
+        m = message.content[6:]
+        ans = test.namu(m)
+        if not ans:
+            e = embed(title = '오류',description = '나무위키에 없는 문서입니다')
+            await app.send_message(message.channel,embed=e)
+        else:
+            e = embed(title = m+" 검색결과",color=0x00A495)
+            e.add_field(name = '내용',value = ans)
+            e.set_footer(text='namu.wiki 나무위키')
+            await app.send_message(message.channel,embed=e)
+
+    if message.content.startswith('!날씨 '):
+        m = message.content[4:]
+        ans = test.weather(m)
+        if not ans:
+            e = embed(title = '오류',description = '존재하지 않는 읍/면/동 입니다')
+            await app.send_message(message.channel,embed=e)
+        else:
+            e = embed(title = m+" 날씨")
+            e.add_field(name = '온도',value = ans.split('\n')[0])
+            e.add_field(name = '공기',value = ans.split('\n')[1])
+            e.set_footer(text='naver.com 네이버')
+            await app.send_message(message.channel,embed=e)
+
+    if message.content.startswith('!영어위키 '):
+        m = message.content[6:]
+        ans = test.enwiki(m)
+        if not ans:
+            e = embed(title = '오류',description = '영어 위키피디아에 없는 문서입니다')
+            await app.send_message(message.channel,embed=e)
+        else:
+            e = embed(title = m+" 검색결과")
+            e.add_field(name = '내용',value = ans)
+            e.set_footer(text='en.wikipedia.org 위키피디아')
+            await app.send_message(message.channel,embed=e)
+    if message.content == '!멜론':
+        e = embed(title='멜론차트 TOP 10',description = '멜론차트 상위 10위를 불러옵니다',color = 0x04D939)
+        ans = test.Melon()
+        for i in range(10):
+            e.add_field(name = i+1,value = ans.tag[i].text+' - '+ans.a[i].text)
         await app.send_message(message.channel,embed=e)
+        
     
 app.run(token)
